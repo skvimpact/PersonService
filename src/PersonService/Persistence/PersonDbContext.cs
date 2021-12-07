edb8a0438f422e3173d87bd196bd01f8c81d84f6
zozo <skvimpact@yandex.ru>
2021-12-08 00:53:23 +0300

    pr

--- src/PersonService/Persistence/PersonDbContext.cs
+++ src/PersonService/Persistence/PersonDbContext.cs
@@ -5,6 +5,11 @@ namespace PersonService.Persistence
     public class PersonDbContext : DbContext
     {
         public PersonDbContext(DbContextOptions<PersonDbContext> options) : base(options) {}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasPostgresExtension("uuid-ossp");
+        }
         public DbSet<Person> Persons {get; set;}
     }
     /*    public class PersonDbContextFactory : IDbContextFactory<PersonDbContext>
--- src/PersonService/Program.cs
+++ src/PersonService/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using PersonService.Persistence;
 
 namespace PersonService
 {
@@ -7,6 +10,20 @@ namespace PersonService
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            PersonRepository repo;
+        
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var optionsBuilder = new DbContextOptionsBuilder<PersonDbContext>();
+            //optionsBuilder.UseNpgsql(config["postgres:cstr"]);
+            optionsBuilder.UseNpgsql("Host=ds220;Port=65432;Database=BeeHive;Username=postgres;Password=zozo");
+                PersonDbContext db = new PersonDbContext(optionsBuilder.Options);
+            repo = new PersonRepository(db);   
+            var p = repo.Get(1);
+            Console.WriteLine(p.Name);         
         }
     }
 }
